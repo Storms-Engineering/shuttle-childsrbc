297b584ea8c0f0bf6c5f12e1d44d40abd36ea9b4
Storm-Engineering <armystorms@gmail.com>
2019-01-16 16:12:05 -0900

    Initial commit

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found).
+ *
+ * @package ShuttleThemes
+ */
+
+get_header(); ?>
+
+	<div class="entry-content title-404">
+		<h2><i class="fa fa-ban"></i><?php _e( '404', 'shuttle' ); ?></h2>
+		
+		<p><?php _e( 'Sorry, Ben could not find the page you are looking for.  Anyways enjoy this funny picture of him.', 'shuttle' ); ?><br/><?php _e( 'Please try using the search function.', 'shuttle' ); ?></p>
+		<?php echo get_search_form(); ?>
+	</div>
+
+<?php get_footer(); ?>
